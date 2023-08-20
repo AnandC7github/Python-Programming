6d0fed2b90e928baa4e7a26d55307b8f57ffc036
Anand.C <122742595+AnandC7github@users.noreply.github.com>
2023-08-20 12:06:48 +0530

    For all non-negative integers i < n, print i^2

---
+++ python1.py
@@ -0,0 +1,6 @@
+#The provided code stub reads and integer, n , from STDIN. For all non-negative integers i < n, print i^2 .
+
+n = int(input())
+if(1 <= n <= 20):
+    for i in range (0,n):
+        print(i*i)
