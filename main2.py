0278fbd3f252c51997ab3f6ad9461f788dad6c14
Anand.C <122742595+AnandC7github@users.noreply.github.com>
2024-01-04 12:47:37 +0530

    Update main2.py
    commented

--- main2.py
+++ main2.py
@@ -1,3 +1,4 @@
+#in this, a new file lib is created under which the file dog.py is inserted. now to run the file we have to include a file named "__init__.py" in the folder and then import the folder name 
 from lib import dog
 
 dog.bark()
